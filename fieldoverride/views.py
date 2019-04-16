825338d130456c0237c0b6e3ff16e1da6cfd0c21
Bob Donahue <bob_donahue@wgbh.org>
2019-04-16 14:47:57 -0400

    And the mixin...

---
+++ fieldoverride/views.py
@@ -0,0 +1,16 @@
+from django.views.generic.detail import SingleObjectMixin
+
+"""
+We'll need a mixin to:
+    - get all the override fields for a model instance
+    - set the context with either the override content (if it exists), OR
+    - the context from the parental fields (in order of precedence)
+"""
+
+class FieldOverrideMixin(SingleObjectMixin):
+    
+    def get_context_data(self, **kwargs):
+        context = super(FieldOverrideMixin, self).get_context_data(**kwargs)
+        
+        # This is where the magic will happen.
+        return context
