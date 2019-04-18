229d148021179ac5e2300e30f33b67ad82f7cc2c
Bob Donahue <bob_donahue@wgbh.org>
2019-04-18 11:16:26 -0400

    Hmmmm.

--- fieldoverride/admin.py
+++ fieldoverride/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericStackedInline
 from .models import FieldOverride
 
+import logging
+django_log = logging.getLogger('django')
+
 class FieldOverrideStackedInline(GenericStackedInline):
     model = FieldOverride
     extra = 0
+    
+    #django_log.info('TEST: {}'.format(model.content_type.model_class().map_override_fields()))
+    # How do I get to?
+    # model.content_type.model_class().map_override_fields()
